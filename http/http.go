eee92a9a44b539195d96ce7771fee6841a892cd1
jiamengwei-ht <mw.jia@foxmail.com>
2021-01-08 15:51:35 +0800

    FileServer

---
+++ http/http.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func main() {
+	//启动一个文件服务器
+	http.Handle("/", http.FileServer(http.Dir("D:/")))
+	http.ListenAndServe(":9999", nil)
+}
---
+++ os/os.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("%0.1s", "123456")
+}
